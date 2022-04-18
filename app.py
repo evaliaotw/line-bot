91349d54b1e4d48d1ba09a00da0965bc980b9efc
evaliaotw <evaliaotw@gmail.com>
2022-04-18 23:10:02 +0800

    update version

--- app.py
+++ app.py
@@ -41,6 +41,17 @@ def handle_message(event):
     msg = event.message.text
     r = '很抱歉,您說什麼?'
 
+    if '給我貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message)        
+        return
+
     if msg in ['hi', 'Hi']:
         r = '嗨'
     elif msg == '你吃飯了嗎':
@@ -51,10 +62,7 @@ def handle_message(event):
         r = '你想訂位，是嗎?'
     line_bot_api.reply_message(
         event.reply_token,
-        StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
-    ))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
